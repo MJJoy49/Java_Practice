035c0c62b23ef00138bbe085b83e386e8fc17e42
M.J.Joy <139280542+MJJoy49@users.noreply.github.com>
2023-10-12 23:33:24 +0600

    Add files via upload

--- Person.java
+++ Person.java
@@ -3,29 +3,114 @@ import java.lang.*;
 public class Person
 {
 	private String name;
+	private int nationalId;
 	private int age;
+	private double height;
 	
-	Person(String name, int age)
+	
+	
+	//------------cons-----------
+	Person(){}
+	Person(String name, int nationalId,int age,double height)
 	{
 		this.name=name;
+		this.nationalId=nationalId;
 		this.age=age;
-		
+		this.height=height;
 	}
+	
+	
+	
+	
+	//------------set---------
+	void setName(String name)
+	{
+		this.name= name;
+	}
+	
+	void setNationalId(int nationalId)
+	{
+		this.nationalId=nationalId;
+	}
+	
+	void setAge(int age)
+	{
+		this.age=age;
+	}
+	
+	void setHeight(double height)
+	{
+		this.height=height;
+	}
+	
+	
+	
+	
+	//-----------get-------
+	String getName()
+	{
+		return name;
+	}
+	
+	int getNationalId()
+	{
+		return nationalId;
+	}
+	
+	int getAge()
+	{
+		return age;
+	}
+	
+	double getHeight()
+	{
+		return height;
+	}
+	
+	
+	
+	
+	//----------showDetails----------
 	
 	void ShowDetails()
 	{
 		System.out.println("The person name: "+name);
+		System.out.println("The person national Id: "+nationalId);
 		System.out.println("The person age: "+age);
+		System.out.println("The person height: "+height);
+		System.out.println(" ");
+		
 	}
 	
 	
+	
+	
+	
+	
+	//------------------main-------------
 	public static void main (String[] args)
 	{
 		
-		
-		Person p1=new Person("maria",19);
+		//--------ob1--------
+		Person p1=new Person("Topu",27733477,19,5.6);
 		p1.ShowDetails();
 		
 		
+		//--------ob2---------
+		Person p2=new Person();
+		p2.setName("Siam");
+		p2.setNationalId(234468264);
+		p2.setAge(20);
+		p2.setHeight(5.8);
+		p2.ShowDetails();
+		
+		
 	}
+	
+	static void Person()
+	{
+		System.out.println("THIS IS PARSOM DATA");
+	}
+	
+
 }
